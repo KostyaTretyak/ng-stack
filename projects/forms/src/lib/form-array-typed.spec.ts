a98cec3b93c70082db14e01a322394d69425f9a5
KostyaTretyak <ktretiak.in.ua@gmail.com>
2019-02-28 23:34:03 +0200

    Refactoring FormArrayTyped

--- projects/forms/src/lib/form-array-typed.spec.ts
+++ projects/forms/src/lib/form-array-typed.spec.ts
@@ -1,29 +1,182 @@
 import { FormArrayTyped } from './form-array-typed';
 import { FormControlTyped } from './form-control-typed';
 import { FormGroupTyped } from './form-group-typed';
-import { isString, isNumber, isObject } from './types';
+import { isString, isNumber, isObject, isArray } from './types';
+
+class Room {
+  whoseBed: string;
+}
 
 class Address {
   region: string;
   district: string;
   city: string;
+  rooms: Room[];
   someNum: number;
 }
 
+// Here problems with type of property rooms
+const formGroup1: FormGroupTyped<Address> = new FormGroupTyped({
+  // notExistingProperty: new FormControlTyped('Mykolaiv'),
+  city: new FormControlTyped('Mykolaiv'),
+  region: new FormControlTyped('Mykolaiv'),
+  // district: new FormGroupTyped({}),
+  district: new FormControlTyped('wat?'),
+  // rooms: new FormControlTyped('Mykolaiv'),
+  // rooms: new FormArrayTyped([new FormControlTyped(123)]),
+  rooms: new FormArrayTyped([
+    new FormControlTyped(123), // <--- Here is a wrong value
+    new FormGroupTyped({
+      whoseBed: new FormControlTyped('my'),
+      one: new FormArrayTyped([]), // <--- Here is a wrong value
+      notExistingProperty: new FormControlTyped(1561), // <--- Here is a wrong value
+    }),
+  ]),
+});
+
+// const rooms1: FormArrayTyped<Room> = new FormArrayTyped([new FormControlTyped(1561)]);
+const rooms2: FormArrayTyped<Room> = new FormArrayTyped([
+  // new FormControlTyped(123),
+  new FormGroupTyped({
+    whoseBed: new FormControlTyped('my'),
+    // one: new FormArrayTyped([]),
+    // notExistingProperty: new FormControlTyped(1561),
+  }),
+]);
+
 const formArray1 = new FormArrayTyped<Address>([
-  // { some: new FormControlTyped('some region') },
-  // new FormControlTyped('some region'),
+  // new FormControlTyped(123),
+  new FormGroupTyped({
+    // notExistingProperty: new FormControlTyped('Mykolaiv'),
+    city: new FormControlTyped('Mykolaiv'),
+    region: new FormControlTyped('Mykolaiv'),
+    district: new FormControlTyped('wat?'),
+    // rooms: new FormControlTyped('Mykolaiv'),
+    rooms: new FormArrayTyped([
+      new FormControlTyped(123), // <--- Here is a wrong value
+      new FormGroupTyped({
+        whoseBed: new FormControlTyped('my'),
+        one: new FormArrayTyped([]), // <--- Here is a wrong value
+        notExistingProperty: new FormControlTyped(1561), // <--- Here is a wrong value
+      }),
+    ]),
+  }),
+]);
+
+const formArray2 = new FormArrayTyped<Address>([
   new FormGroupTyped({
     city: new FormControlTyped('Kyiv'),
     region: new FormControlTyped('Kyiv'),
     district: new FormControlTyped('wat?'),
+    rooms: new FormArrayTyped([
+      new FormControlTyped(123),
+      new FormGroupTyped({
+        whoseBed: new FormControlTyped('my'),
+        one: new FormArrayTyped([]),
+        notExistingProperty: new FormControlTyped(1561),
+      }),
+    ]),
+  }),
+  new FormGroupTyped({
+    city: new FormControlTyped('Mykolaiv'),
+    region: new FormControlTyped('Mykolaiv'),
+    district: new FormControlTyped('wat?'),
+  }),
+  new FormGroupTyped({
+    city: new FormControlTyped('Kharkiv'),
+    region: new FormControlTyped('Kharkiv'),
+    district: new FormControlTyped('wat?'),
   }),
 ]);
 
-formArray1.value.forEach(address => {
+formArray2.value.forEach(address => {
   isObject(address);
   isString(address.region);
   isString(address.district);
   isString(address.city);
   isNumber(address.someNum);
 });
+
+const control0 = formArray2.at(0);
+const addr = control0.value;
+isObject(addr);
+isString(addr.region);
+isString(addr.district);
+isString(addr.city);
+isNumber(addr.someNum);
+
+// formArray2.push({});
+formArray2.push(control0);
+// formArray2.insert(2, {});
+formArray2.insert(2, control0);
+// formArray2.setControl(2, {});
+formArray2.setControl(2, control0);
+
+formArray2.setValue([
+  { city: 'Kyiv', region: 'Kyiv', district: 'wat?', rooms: [], someNum: 1 },
+  // { city: 21, region: 'Mykolaiv', district: 'wat?', rooms: '', someNum: '' },
+  { city: 'Mykolaiv', region: 'Mykolaiv', district: 'wat?', rooms: [], someNum: 1 },
+  { city: 'Kharkiv', region: 'Kharkiv', district: 'wat?', rooms: [], someNum: 1 },
+]);
+
+formArray2.patchValue([
+  { city: 'Kyiv', region: 'Kyiv', district: 'wat?', rooms: [], someNum: 1 },
+  // { city: 21, region: 'Mykolaiv', district: 'wat?', rooms: '', someNum: '' },
+  { city: 'Mykolaiv', region: 'Mykolaiv', district: 'wat?', rooms: [], someNum: 1 },
+  { city: 'Kharkiv', region: 'Kharkiv', district: 'wat?', rooms: [], someNum: 1 },
+]);
+
+formArray2.reset([
+  { city: 'Kyiv', region: 'Kyiv', district: 'wat?', rooms: [], someNum: 1 },
+  // { city: 21, region: 'Mykolaiv', district: 'wat?', rooms: '', someNum: '' },
+  { value: { city: 'Mykolaiv', region: 'Mykolaiv', district: 'wat?', rooms: [], someNum: 1 }, disabled: true },
+  { city: 'Kharkiv', region: 'Kharkiv', district: 'wat?', rooms: [], someNum: 1 },
+]);
+
+isArray(formArray2.getRawValue());
+
+formArray2.getRawValue().forEach(address => {
+  isObject(address);
+  isString(address.region);
+  isString(address.district);
+  isString(address.city);
+  isNumber(address.someNum);
+});
+
+// An array in an array
+const formArray3 = new FormArrayTyped<Address[]>([
+  // new FormControlTyped(123),
+  new FormArrayTyped([
+    // new FormControlTyped(123),
+    new FormGroupTyped({
+      city: new FormControlTyped('Mykolaiv'),
+      region: new FormControlTyped('Mykolaiv'),
+      district: new FormControlTyped('wat?'),
+      rooms: new FormArrayTyped([
+        new FormControlTyped(123), // <--- Here is a wrong value
+        new FormGroupTyped({
+          whoseBed: new FormControlTyped('my'),
+          one: new FormArrayTyped([]), // <--- Here is a wrong value
+          notExistingProperty: new FormControlTyped(1561), // <--- Here is a wrong value
+        }),
+      ]),
+    }),
+  ]),
+]);
+
+const control1 = formArray3.at(0);
+isArray(control1.value);
+control1.value.forEach(address => {
+  isObject(address);
+  isString(address.region);
+  isString(address.district);
+  isString(address.city);
+  isNumber(address.someNum);
+});
+
+// formArray3.push({});
+formArray3.push(control1);
+// formArray3.insert(2, {});
+formArray3.insert(2, control1);
+// formArray3.setControl(2, {});
+formArray3.setControl(2, control1);
--- projects/forms/src/lib/form-array-typed.ts
+++ projects/forms/src/lib/form-array-typed.ts
@@ -1,33 +1,205 @@
 import { FormArray, ValidatorFn, AbstractControlOptions, AsyncValidatorFn } from '@angular/forms';
 
-import { ControlOfFormArray, OnlyIndexOf } from './types';
+import { ControlOfFormArray } from './types';
 import { Observable } from 'rxjs';
 
-export class FormArrayTyped<T extends object = any> extends FormArray {
-  value: T[];
-  valueChanges: Observable<T[]>;
+export class FormArrayTyped<Item = any> extends FormArray {
+  value: Item[];
+  valueChanges: Observable<Item[]>;
 
+  /**
+   * Creates a new `FormArray` instance.
+   *
+   * @param controls An array of child controls. Each child control is given an index
+   * where it is registered.
+   *
+   * @param validatorOrOpts A synchronous validator function, or an array of
+   * such functions, or an `AbstractControlOptions` object that contains validation functions
+   * and a validation trigger.
+   *
+   * @param asyncValidator A single async validator or array of async validator functions
+   *
+   */
   constructor(
-    public controls: ControlOfFormArray<T, OnlyIndexOf<T>>[],
+    public controls: ControlOfFormArray<Item>[],
     validatorOrOpts?: ValidatorFn | ValidatorFn[] | AbstractControlOptions | null,
     asyncValidator?: AsyncValidatorFn | AsyncValidatorFn[] | null
   ) {
     super(controls, validatorOrOpts, asyncValidator);
   }
 
+  /**
+   * Get the Control at the given `index` in the array.
+   *
+   * @param index Index in the array to retrieve the control
+   */
   at(index: number) {
-    return super.at(index) as ControlOfFormArray<T, OnlyIndexOf<T>>;
+    return super.at(index) as ControlOfFormArray<Item>;
   }
 
-  push(control: ControlOfFormArray<T, OnlyIndexOf<T>>) {
+  /**
+   * Insert a new Control at the end of the array.
+   *
+   * @param control Form control to be inserted
+   */
+  push(control: ControlOfFormArray<Item>) {
     return super.push(control);
   }
 
-  insert(index: number, control: ControlOfFormArray<T, OnlyIndexOf<T>>) {
+  /**
+   * Insert a new Control at the given `index` in the array.
+   *
+   * @param index Index in the array to insert the control
+   * @param control Form control to be inserted
+   */
+  insert(index: number, control: ControlOfFormArray<Item>) {
     return super.insert(index, control);
   }
 
-  setControl(index: number, control: ControlOfFormArray<T, OnlyIndexOf<T>>) {
+  /**
+   * Replace an existing control.
+   *
+   * @param index Index in the array to replace the control
+   * @param control The Control control to replace the existing control
+   */
+  setControl(index: number, control: ControlOfFormArray<Item>) {
     return super.setControl(index, control);
+  }
+
+  /**
+   * Sets the value of the `FormArray`. It accepts an array that matches
+   * the structure of the control.
+   *
+   * This method performs strict checks, and throws an error if you try
+   * to set the value of a control that doesn't exist or if you exclude the
+   * value of a control.
+   *
+   * ### Set the values for the controls in the form array
+   *
+```ts
+const arr = new FormArray([
+  new FormControl(),
+  new FormControl()
+]);
+console.log(arr.value);   // [null, null]
+
+arr.setValue(['Nancy', 'Drew']);
+console.log(arr.value);   // ['Nancy', 'Drew']
+```
+   *
+   * @param value Array of values for the controls
+   * @param options Configure options that determine how the control propagates changes and
+   * emits events after the value changes
+   *
+   * * `onlySelf`: When true, each change only affects this control, and not its parent. Default
+   * is false.
+   * * `emitEvent`: When true or not supplied (the default), both the `statusChanges` and
+   * `valueChanges`
+   * observables emit events with the latest status and value when the control value is updated.
+   * When false, no events are emitted.
+   * The configuration options are passed to the
+   * [updateValueAndValidity](https://angular.io/api/forms/AbstractControl#updateValueAndValidity) method.
+   */
+  setValue(value: Item[], options: { onlySelf?: boolean; emitEvent?: boolean } = {}) {
+    return super.setValue(value, options);
+  }
+
+  /**
+   * Patches the value of the `FormArray`. It accepts an array that matches the
+   * structure of the control, and does its best to match the values to the correct
+   * controls in the group.
+   *
+   * It accepts both super-sets and sub-sets of the array without throwing an error.
+   *
+   * ### Patch the values for controls in a form array
+   *
+```ts
+const arr = new FormArray([
+   new FormControl(),
+   new FormControl()
+]);
+console.log(arr.value);   // [null, null]
+
+arr.patchValue(['Nancy']);
+console.log(arr.value);   // ['Nancy', null]
+```
+   *
+   * @param value Array of latest values for the controls
+   * @param options Configure options that determine how the control propagates changes and
+   * emits events after the value changes
+   *
+   * * `onlySelf`: When true, each change only affects this control, and not its parent. Default
+   * is false.
+   * * `emitEvent`: When true or not supplied (the default), both the `statusChanges` and
+   * `valueChanges`
+   * observables emit events with the latest status and value when the control value is updated.
+   * When false, no events are emitted.
+   * The configuration options are passed to the
+   * [updateValueAndValidity](https://angular.io/api/forms/AbstractControl#updateValueAndValidity) method.
+   */
+  patchValue(value: Item[], options: { onlySelf?: boolean; emitEvent?: boolean } = {}) {
+    return super.patchValue(value, options);
+  }
+
+  /**
+   * Resets the `FormArray` and all descendants are marked `pristine` and `untouched`, and the
+   * value of all descendants to null or null maps.
+   *
+   * You reset to a specific form state by passing in an array of states
+   * that matches the structure of the control. The state is a standalone value
+   * or a form state object with both a value and a disabled status.
+   *
+   * ### Reset the values in a form array
+   *
+```ts
+const arr = new FormArray([
+   new FormControl(),
+   new FormControl()
+]);
+arr.reset(['name', 'last name']);
+
+console.log(this.arr.value);  // ['name', 'last name']
+```
+   *
+   * ### Reset the values in a form array and the disabled status for the first control
+   *
+```
+this.arr.reset([
+  {value: 'name', disabled: true},
+  'last'
+]);
+
+console.log(this.arr.value);  // ['name', 'last name']
+console.log(this.arr.get(0).status);  // 'DISABLED'
+```
+   *
+   * @param value Array of values for the controls
+   * @param options Configure options that determine how the control propagates changes and
+   * emits events after the value changes
+   *
+   * * `onlySelf`: When true, each change only affects this control, and not its parent. Default
+   * is false.
+   * * `emitEvent`: When true or not supplied (the default), both the `statusChanges` and
+   * `valueChanges`
+   * observables emit events with the latest status and value when the control is reset.
+   * When false, no events are emitted.
+   * The configuration options are passed to the
+   * [updateValueAndValidity](https://angular.io/api/forms/AbstractControl#updateValueAndValidity) method.
+   */
+  reset(
+    value: (Item | { value: Item; disabled: boolean })[] = [],
+    options: { onlySelf?: boolean; emitEvent?: boolean } = {}
+  ) {
+    return super.reset(value, options);
+  }
+
+  /**
+   * The aggregate value of the array, including any disabled controls.
+   *
+   * Reports all values regardless of disabled status.
+   * For enabled controls only, the `value` property is the best way to get the value of the array.
+   */
+  getRawValue() {
+    return super.getRawValue() as Item[];
   }
 }
