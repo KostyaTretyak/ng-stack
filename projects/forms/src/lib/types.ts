1977b790e1b4c00460c48bc0a116ab11d1a31c08
KostyaTretyak <ktretiak.in.ua@gmail.com>
2019-02-28 23:35:27 +0200

    Refactoring TS types for forms

--- projects/forms/src/lib/types.ts
+++ projects/forms/src/lib/types.ts
@@ -8,17 +8,16 @@ export interface ObjectAny {
   [key: string]: any;
 }
 
-export type OnlyStringKeyOf<T> = Extract<keyof T, string>;
-export type OnlyIndexOf<T> = Extract<keyof T, number>;
+export type StringKeys<T> = Extract<keyof T, string>;
 
-export type ControlOfFormGroup<T, K extends OnlyStringKeyOf<T>> = T[K] extends object[]
-  ? FormArrayTyped<T[K][0]>
+export type ControlOfFormGroup<T extends object, K extends StringKeys<T>> = T[K] extends (infer Item)[]
+  ? FormArrayTyped<Item>
   : T[K] extends object
   ? FormGroupTyped<T[K]>
   : FormControlTyped<T[K]>;
 
-export type ControlOfFormArray<T, K extends OnlyIndexOf<T>> = T extends object[]
-  ? FormArrayTyped<T[K]>
+export type ControlOfFormArray<T> = T extends (infer Item)[]
+  ? FormArrayTyped<Item>
   : T extends object
   ? FormGroupTyped<T>
   : FormControlTyped<T>;
@@ -26,17 +25,17 @@ export type ControlOfFormArray<T, K extends OnlyIndexOf<T>> = T extends object[]
 export type ControlFormState<T> = T | { value: T; disable: boolean };
 
 // Form builder control state
-export type FbControlFormState<T, K> = K extends OnlyStringKeyOf<T> ? T[K] | { value: T[K]; disabled: boolean } : any;
+export type FbControlFormState<T, K> = K extends StringKeys<T> ? T[K] | { value: T[K]; disabled: boolean } : any;
 
 // Form builder control config
 export type FbControlsConfig<T> = {
-  [P in OnlyStringKeyOf<T>]:
+  [P in StringKeys<T>]:
     | T[P]
     | [T[P], (ValidatorFn | ValidatorFn[] | AbstractControlOptions)?, (AsyncValidatorFn | AsyncValidatorFn[])?]
 };
 
 // Form builder control returns
-export type FbControlReturns<T, K> = K extends OnlyStringKeyOf<T> ? T[K] : any;
+export type FbControlReturns<T, K> = K extends StringKeys<T> ? T[K] : any;
 
 export interface UpdateValueOptions {
   onlySelf?: boolean;
@@ -46,15 +45,17 @@ export interface UpdateValueOptions {
 // Assertions type only.
 
 export type Diff<T, X> = T extends X ? never : T;
-export type Filter<T, X> = T extends X ? T : never;
 
-export type hasType<T, U> = Diff<T, Filter<T, never>> & U;
+export type hasType<T, U> = Diff<T, Extract<T, never>> & U;
 export type Fn = (...args: any[]) => any;
 export type NonFn<T> = Diff<T, Fn>;
+export type IsArray<T> = T extends (infer Item)[] ? Item : never;
+export type NonArray<T> = Diff<T, IsArray<T>>;
 
 export declare function isString<T>(value: hasType<T, string>): string;
 export declare function isNumber<T>(value: hasType<T, number>): number;
 export declare function isBoolean<T>(value: hasType<T, boolean>): boolean;
 export declare function isSymbol<T>(value: hasType<T, symbol>): symbol;
 export declare function isFunction<T>(value: hasType<T, Fn>): Fn;
-export declare function isObject<T>(value: hasType<NonFn<T>, object>): object;
+export declare function isArray<T>(value: hasType<T, any[]>): any[];
+export declare function isObject<T>(value: hasType<NonFn<T> & NonArray<T>, object>): object;
