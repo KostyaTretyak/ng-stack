1db7c6b869880c890f88336705d86023f2e86acc
KostyaTretyak <ktretiak.in.ua@gmail.com>
2019-03-05 19:44:59 +0200

    Removed AbstractControlTyped from exports

--- projects/forms/src/public_api.ts
+++ projects/forms/src/public_api.ts
@@ -1,7 +1,6 @@
 /*
  * Public API Surface of forms
  */
-export { AbstractControlTyped as AbstractControl } from './lib/abstract-control-typed';
 export { FormArrayTyped as FormArray } from './lib/form-array-typed';
 export { FormBuilderTyped as FormBuilder } from './lib/form-builder-typed';
 export { FormControlTyped as FormControl } from './lib/form-control-typed';
