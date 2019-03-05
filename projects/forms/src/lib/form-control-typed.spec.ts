05c688d92a25cf8da4e8a7f749591ee467f0cf60
KostyaTretyak <ktretiak.in.ua@gmail.com>
2019-03-05 19:04:07 +0200

    refactoring(forms): tests of FormControl

--- projects/forms/src/lib/form-control-typed.spec.ts
+++ projects/forms/src/lib/form-control-typed.spec.ts
@@ -3,6 +3,77 @@ import { isString, isNumber, isArray } from './assert';
 import { FormGroupTyped as FormGroup } from './form-group-typed';
 
 describe('FormControl', () => {
+  xdescribe('checking types only', () => {
+    it('constructor()', () => {
+      isString(new FormControl('').value);
+      isString(new FormControl<string>().value);
+      isNumber(new FormControl(1).value);
+      isNumber(new FormControl<number>().value);
+      isArray(new FormControl([]).value);
+      isArray(new FormControl<number[]>().value);
+
+      const formState1 = { value: '', disabled: false };
+      const control1 = new FormControl(formState1);
+      isString(control1.value);
+
+      const formState2 = { value: 2, disabled: false };
+      const control2 = new FormControl(formState2);
+      isNumber(control2.value);
+
+      const formState3 = { value: [], disabled: false };
+      const control3 = new FormControl(formState3);
+      isArray(control3.value);
+    });
+
+    it('setValue(), patchValue(), reset()', () => {
+      const control1 = new FormControl('');
+      control1.setValue('');
+      // control1.setValue(2);
+      // control1.setValue([]);
+      control1.patchValue('');
+      // control1.patchValue(2);
+      // control1.patchValue([]);
+      control1.reset('');
+      // control1.reset(2);
+      // control1.reset([]);
+
+      const control2 = new FormControl(2);
+      control2.setValue(2);
+      // control2.setValue('');
+      // control2.setValue([]);
+      control2.patchValue(2);
+      // control2.patchValue('');
+      // control2.patchValue([]);
+      control2.reset(2);
+      // control2.reset('');
+      // control2.reset([]);
+
+      const formState3 = { value: '', disabled: false };
+      const control3 = new FormControl(formState3);
+      control3.setValue('');
+      // control3.setValue(2);
+      // control3.setValue([]);
+      control3.patchValue('');
+      // control3.patchValue(2);
+      // control3.patchValue([]);
+      control3.reset('');
+      // control3.reset(2);
+      // control3.reset([]);
+
+      const formState4 = { value: 2, disabled: false };
+      const control4 = new FormControl(formState4);
+      control4.setValue(2);
+      // control4.setValue('');
+      // control4.setValue([]);
+      control4.patchValue(2);
+      // control4.patchValue('');
+      // control4.patchValue([]);
+      control4.reset(2);
+      // control4.reset('');
+      // control4.reset([]);
+    });
+  });
+
   describe(`checking runtime work`, () => {
     describe(`constructor()`, () => {
       it('passing primitive types as params', () => {
@@ -75,77 +146,6 @@ describe('FormControl', () => {
         expect((control as any).get()).toBe(null);
         expect((control as any).get('one')).toBe(null);
       });
-    });
-  });
-
-  xdescribe('checking types only', () => {
-    it('inferring type from constructor params', () => {
-      isString(new FormControl('').value);
-      isString(new FormControl<string>().value);
-      isNumber(new FormControl(1).value);
-      isNumber(new FormControl<number>().value);
-      isArray(new FormControl([]).value);
-      isArray(new FormControl<number[]>().value);
-
-      const formState1 = { value: '', disabled: false };
-      const control1 = new FormControl(formState1);
-      isString(control1.value);
-
-      const formState2 = { value: 2, disabled: false };
-      const control2 = new FormControl(formState2);
-      isNumber(control2.value);
-
-      const formState3 = { value: [], disabled: false };
-      const control3 = new FormControl(formState3);
-      isArray(control3.value);
-    });
-
-    it('should accept allowed parameters only', () => {
-      const control1 = new FormControl('');
-      control1.setValue('');
-      // control1.setValue(2);
-      // control1.setValue([]);
-      control1.patchValue('');
-      // control1.patchValue(2);
-      // control1.patchValue([]);
-      control1.reset('');
-      // control1.reset(2);
-      // control1.reset([]);
-
-      const control2 = new FormControl(2);
-      control2.setValue(2);
-      // control2.setValue('');
-      // control2.setValue([]);
-      control2.patchValue(2);
-      // control2.patchValue('');
-      // control2.patchValue([]);
-      control2.reset(2);
-      // control2.reset('');
-      // control2.reset([]);
-
-      const formState3 = { value: '', disabled: false };
-      const control3 = new FormControl(formState3);
-      control3.setValue('');
-      // control3.setValue(2);
-      // control3.setValue([]);
-      control3.patchValue('');
-      // control3.patchValue(2);
-      // control3.patchValue([]);
-      control3.reset('');
-      // control3.reset(2);
-      // control3.reset([]);
-
-      const formState4 = { value: 2, disabled: false };
-      const control4 = new FormControl(formState4);
-      control4.setValue(2);
-      // control4.setValue('');
-      // control4.setValue([]);
-      control4.patchValue(2);
-      // control4.patchValue('');
-      // control4.patchValue([]);
-      control4.reset(2);
-      // control4.reset('');
-      // control4.reset([]);
     });
   });
 });
