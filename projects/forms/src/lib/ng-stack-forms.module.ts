905feb4c7f9c4adc5a4a448d8d220f91d557364c
KostyaTretyak <ktretiak.in.ua@gmail.com>
2019-03-06 09:38:48 +0200

    refactoring(forms): reexport ReactiveFormsModule

--- projects/forms/src/lib/ng-stack-forms.module.ts
+++ projects/forms/src/lib/ng-stack-forms.module.ts
@@ -4,7 +4,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { FormBuilder } from './form-builder';
 
 @NgModule({
-  imports: [ReactiveFormsModule],
+  exports: [ReactiveFormsModule],
   providers: [FormBuilder],
 })
 export class NgStackFormsModule {}
