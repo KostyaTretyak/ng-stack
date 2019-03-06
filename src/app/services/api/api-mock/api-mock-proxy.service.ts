2111881f8898aaf53cae9afbd1bdec68d1a0c6cd
KostyaTretyak <ktretiak.in.ua@gmail.com>
2019-03-06 20:26:51 +0200

    chore(default): restored PostsCommentsService

--- src/app/services/api/api-mock/api-mock-proxy.service.ts
+++ src/app/services/api/api-mock/api-mock-proxy.service.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@angular/core';
 
-import { ApiMockRouteGroup, ApiMockService } from '@ng-stack/api-mock';
+import { ApiMockService, ApiMockRouteGroup } from '@ng-stack/api-mock';
 
+import { PostsCommentsService } from './posts-comments.service';
 import { SimpleService } from './simple.service';
 
+/**
+ * If we have many groups of routes, we need the proxy service for more readable code.
+ */
 @Injectable()
-export class ApiMockProxyService implements ApiMockService {
-  constructor(private simpleService: SimpleService) {}
+export class ProxyApiMockService implements ApiMockService {
+  constructor(private simpleService: SimpleService, private postCommentService: PostsCommentsService) {}
 
   getRouteGroups(): ApiMockRouteGroup[] {
-    return [...this.simpleService.getRouteGroups()];
+    return [...this.simpleService.getRouteGroups(), ...this.postCommentService.getRouteGroups()];
   }
 }
---
+++ src/app/services/api/api-mock/types.ts
@@ -0,0 +1,66 @@
+import { ObjectAny } from '../../../types/mix';
+
+/**
+ * Here we need init properties with `null`
+ * for use it for `pickAllPropertiesAsGetters()` util as list of properties.
+ */
+export class PostList {
+  postId: number = null;
+  userName: string = null;
+  postTitle: string = null;
+  postLead: string = null;
+  pathAva: string = null;
+  countComments: number = null;
+  datePosted: number = null;
+  lastCommentators?: Commentator[] = [];
+}
+
+export class Post extends PostList {
+  userId: number = null;
+  postBody: string = null;
+}
+
+export class Commentator {
+  userId: number = null;
+  userName: string = null;
+  pathAva: string = null;
+}
+
+export class PostComment {
+  commentId: number = null;
+  userId: number = null;
+  parentId: number = null;
+  userName: string = null;
+  postId: number = null;
+  dateInsert: number = null;
+  dateUpdate: number = null;
+  pathAva: string = null;
+  commentBody: string = null;
+}
+
+export class PostInfo {
+  authorshipId: number = null;
+  pathAva: string = null;
+  datePosted: number = null;
+  postId: number = null;
+  postLead: string = null;
+  postTitle: string = null;
+  userName: string = null;
+  userId: number = null;
+  countComments: number = null;
+}
+
+export interface ApiResponse<T, M = ObjectAny> {
+  data?: T[];
+  error?: ObjectAny;
+  meta?: M;
+}
+
+export class Customer {
+  customerId: number = null;
+}
+
+export class Order {
+  orderId: number = null;
+  customerId: number = null;
+}
