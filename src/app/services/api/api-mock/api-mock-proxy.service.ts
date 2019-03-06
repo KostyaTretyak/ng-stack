69eb8869842353857200988dd4843f06886d1f70
KostyaTretyak <ktretiak.in.ua@gmail.com>
2019-03-06 20:30:04 +0200

    fix(default): fixed name ApiMockProxyService

--- src/app/services/api/api-mock/api-mock-proxy.service.ts
+++ src/app/services/api/api-mock/api-mock-proxy.service.ts
@@ -9,7 +9,7 @@ import { SimpleService } from './simple.service';
  * If we have many groups of routes, we need the proxy service for more readable code.
  */
 @Injectable()
-export class ProxyApiMockService implements ApiMockService {
+export class ApiMockProxyService implements ApiMockService {
   constructor(private simpleService: SimpleService, private postCommentService: PostsCommentsService) {}
 
   getRouteGroups(): ApiMockRouteGroup[] {
