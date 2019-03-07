e8f6b24fa1cc98d71117bf01318368c7a312ccbe
KostyaTretyak <ktretiak.in.ua@gmail.com>
2019-03-07 20:54:20 +0200

    chore(forms): added ValidatorsReturns to exports

--- projects/forms/src/public_api.ts
+++ projects/forms/src/public_api.ts
@@ -7,4 +7,12 @@ export { FormBuilder } from './lib/form-builder';
 export { FormControl } from './lib/form-control';
 export { FormGroup } from './lib/form-group';
 export { Validators } from './lib/validators';
-export { Control, Status, ValidatorFn, AsyncValidatorFn, ValidationErrors, AbstractControlOptions } from './lib/types';
+export {
+  Control,
+  Status,
+  ValidatorFn,
+  AsyncValidatorFn,
+  ValidationErrors,
+  AbstractControlOptions,
+  ValidatorsReturns,
+} from './lib/types';
