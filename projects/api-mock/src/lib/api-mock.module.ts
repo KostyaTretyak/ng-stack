cf10773dbc3c805cb85626b666bdadab2db110c7
KostyaTretyak <ktretiak.in.ua@gmail.com>
2019-04-06 18:09:01 +0300

    Release api-mock@0.0.0-alpha.13

--- projects/api-mock/src/lib/api-mock.module.ts
+++ projects/api-mock/src/lib/api-mock.module.ts
@@ -1,13 +1,10 @@
 import { HttpBackend } from '@angular/common/http';
 import { ModuleWithProviders, NgModule, Type } from '@angular/core';
-import { RouterModule } from '@angular/router';
 
 import { HttpBackendService } from './http-backend.service';
 import { ApiMockConfig, ApiMockService } from './types';
 
-@NgModule({
-  imports: [RouterModule],
-})
+@NgModule()
 export class ApiMockModule {
   static forRoot(apiMockService: Type<ApiMockService>, apiMockConfig?: ApiMockConfig): ModuleWithProviders {
     return {
